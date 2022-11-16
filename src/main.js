40105b3b92a73e661de65b58a288c14eec14574e
Tiosphere <loong_22@hotmail.com>
2022-11-16 20:13:27 +0700

    add vue-router and config

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ import { createApp } from 'vue'
 // Plugins
 import { registerPlugins } from '@/plugins'
 import vuetify from './plugins/vuetify'
+import router from './router'
 
 const app = createApp(App)
 
@@ -20,4 +21,5 @@ registerPlugins(app)
 
 app
   .use(vuetify)
+  .use(router)
   .mount('#app')
