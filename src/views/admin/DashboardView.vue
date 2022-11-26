4584c1b939c7bc57f6b98b3f1000fb64eee6468f
YanggggAng <tassapon.ans@student.mahidol.edu>
2022-11-27 00:27:00 +0700

    change init wireframe of admin page.

--- src/views/admin/DashboardView.vue
+++ src/views/admin/DashboardView.vue
@@ -1,30 +1,98 @@
-
-
 <template>
     <v-app id="inspire">
-      <v-navigation-drawer
-        model-value
-        class="pt-4"
-        color="grey-lighten-3"
-        rail
-      >
-        <v-avatar
-          v-for="n in 6"
-          :key="n"
-          :color="`grey-${n === 1 ? 'darken' : 'lighten'}-1`"
-          :size="n === 1 ? 36 : 20"
-          class="d-block text-center mx-auto mb-9"
-        ></v-avatar>
+  
+      <v-navigation-drawer v-model="drawer">
+        <v-sheet
+          color="grey-lighten-4"
+          class="pa-4"
+        >
+          <v-avatar
+            class="mb-4"
+            color="grey-darken-1"
+            size="64"
+          ></v-avatar>
+  
+          <div>Admin Panel</div>
+        </v-sheet>
+  
+        <v-divider></v-divider>
+  
+        <v-list>
+          <v-list-item
+            :to="url"
+            v-for="[icon, text, url] in links"
+            :key="icon"
+            link
+          >
+          
+            <template v-slot:prepend>
+                <!-- <RouterLink :to="{ name: '{{ url }}' }"> -->
+                    <v-icon>{{ icon }}</v-icon>
+                <!-- </RouterLink> -->
+            </template>
+  
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          
+          </v-list-item>
+        </v-list>
       </v-navigation-drawer>
   
       <v-main>
-        <!--  -->
+        <v-container
+          class="py-8 px-6"
+          fluid
+        >
+          <v-row>
+            <v-col
+              v-for="card in cards"
+              :key="card"
+              cols="12"
+            >
+              <v-card>
+  
+                <v-list lines="two">
+                  <v-list-subheader>{{ card }}</v-list-subheader>
+                  <template v-for="n in 6" :key="n">
+                    <v-list-item>
+                      <template v-slot:prepend>
+                        <v-avatar color="grey-darken-1"></v-avatar>
+                      </template>
+  
+                      <v-list-item-title>Message {{ n }}</v-list-item-title>
+  
+                      <v-list-item-subtitle>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique
+                      </v-list-item-subtitle>
+                    </v-list-item>
+  
+                    <v-divider
+                      v-if="n !== 6"
+                      :key="`divider-${n}`"
+                      inset
+                    ></v-divider>
+                  </template>
+                </v-list>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-main>
     </v-app>
   </template>
   
   <script>
     export default {
-      //
+      data: () => ({
+        cards: ['Today', 'Yesterday'],
+        drawer: null,
+        links: [
+
+          ['mdi-account-group','Manage Accounts', '/admin/manage'],
+          ['mdi-inbox-arrow-down', 'Inbox', ''],
+          ['mdi-send', 'Send', ''],
+          ['mdi-delete', 'Trash', ''],
+          ['mdi-alert-octagon', 'Spam', ''],
+        ],
+      }),
     }
   </script>
