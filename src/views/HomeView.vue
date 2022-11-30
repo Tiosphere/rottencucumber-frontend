0108cebd5011942de21268936970ece4786c7a5f
YanggggAng <tassapon.ans@student.mahidol.edu>
2022-11-30 15:23:00 +0700

    prepare the var for additon movie in each type.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,12 +18,10 @@
     </v-col>
 
       <flickity class="flickity" ref="flickity" :options="flickityOptions"
-      
       >
         <div class="carousel-cell"
-        v-for="movie in movies.sort((a,b) => {
+        v-for="movie in popular_movies.sort((a,b) => {
           return a.view - b.view}).reverse()">
-        
         <v-img
             :aspect-ratio="3/4"
             class="mx-auto bg-white"
@@ -61,7 +59,7 @@
       
       >
         <div class="carousel-cell"
-        v-for="movie in movies.sort((a,b) => {
+        v-for="movie in lastest_movies.sort((a,b) => {
           return a.view - b.view}).reverse()">
         
         <v-img
@@ -79,31 +77,6 @@
         </div>
       </flickity>
     </div>
-
-
-    <!-- <div style="padding: 100px">
-
-      <flickity class="flickity" ref="flickity" :options="flickityOptions"
-      >
-        <div class="carousel-cell">
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg" alt="orange tree" />
-        </div>
-        <div class="carousel-cell">
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg" alt="submerged" />
-        </div>
-        <div class="carousel-cell">
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg" alt="look-out" />
-        </div>
-        <div class="carousel-cell">
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/one-world-trade.jpg" alt="One World Trade" />
-        </div>
-        <div class="carousel-cell">
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/drizzle.jpg" alt="drizzle" />
-        </div>
-      </flickity>
-
-    </div> -->
-
     <foot />
   </template>
 
@@ -133,29 +106,7 @@ export default {
         groupCells: true,
         groupCells: 2
       },
-      moviesPic: [
-        {
-          name: 'Movie1',
-          url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/orange-tree.jpg'
-        },
-        {
-          name: 'Movie2',
-          url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/submerged.jpg'
-        },
-        {
-          name: 'Movie3',
-          url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/look-out.jpg'
-        },
-        {
-          name: 'Movie4',
-          url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/one-world-trade.jpg'
-        },
-        {
-          name: 'Movie5',
-          url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/82/drizzle.jpg'
-        },
-      ],
-      movies: [
+      popular_movies: [
       {
         title: 'Man of steel',
         src: 'https://resizing.flixster.com/ownHNmxE8MDUccJXu9znLuvRr1E=/206x305/v2/https://flxt.tmsimg.com/assets/p9259673_p_v8_ag.jpg',
@@ -188,6 +139,55 @@ export default {
       {
         title: 'Lilo & Stitch', src: 'https://m.media-amazon.com/images/I/51pgU6o91KL._AC_SY780_.jpg',
         year: '2002', view: '5000'
+      },
+    ],
+    lastest_movies: [
+    {
+        title: 'Man of steel',
+        src: 'https://resizing.flixster.com/ownHNmxE8MDUccJXu9znLuvRr1E=/206x305/v2/https://flxt.tmsimg.com/assets/p9259673_p_v8_ag.jpg',
+        year: '2013', month: '8', day: '12',
+        view: '15000'
+      },
+      {
+        title: 'Spiderman no way home',
+        src: 'https://cdn.majorcineplex.com/uploads/content/images/20220823120749_Fav5_AcUsAAtaUL.jpeg',
+        year: '2021', month: '6', day: '12',
+        view: '20000'
+      },
+      {
+        title: 'Mulan',
+        src: 'https://i0.wp.com/jasonsmovieblog.com/wp-content/uploads/2020/09/mulan-598495111d425.jpg?resize=361%2C514&ssl=1',
+        year: '1998', month: '4', day: '12',
+        view: '35000'
+      },
+      {
+        title: 'Titanic',
+        src: 'https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg',
+        year: '1997', month: '11', day: '12',
+        view: '1000'
+      },
+      {
+        title: 'Bolt',
+        src: 'https://images.moviesanywhere.com/5d927f6b6564c5f2d54c74fca39af6d4/6ae6ffba-1392-464c-b9ca-8a917f5df789.jpg',
+        year: '2008', month: '1', day: '12',
+        view: '4000'
+      },
+      {
+        title: 'Lilo & Stitch', src: 'https://m.media-amazon.com/images/I/51pgU6o91KL._AC_SY780_.jpg',
+        year: '2002', month: '5', day: '12',
+        view: '5000'
+      },
+      {
+        title: 'The Avengers',
+        src: 'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg',
+        year: '2012', month: '7', day: '12',
+        view: '80000'
+      },
+      {
+        title: 'Wonder Woman',
+        src: 'https://m.media-amazon.com/images/M/MV5BMTYzODQzYjQtNTczNC00MzZhLTg1ZWYtZDUxYmQ3ZTY4NzA1XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_FMjpg_UX1000_.jpg',
+        year: '2017', month: '1', day: '12',
+        view: '50000'
       },
     ]
 
